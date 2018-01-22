c07501ccf483370a93ae057ef9f738fee5678a65
osiris <shigotoganbatte@gmail.com>
2018-01-22 13:29:39 +0800

    test

--- src/main/java/com/edurekademo/utilities/StringUtilities.java
+++ src/main/java/com/edurekademo/utilities/StringUtilities.java
@@ -17,7 +17,7 @@ import org.slf4j.LoggerFactory;
  * An utility class that is used to split an string into an array list and providing mechanism to 
  * build a HashMap value based on a variable number of arguements. 
  * @author Seshagiri Sriram
- * @version 1.2 
+ * @version 1.3 
  *
  */
 public class StringUtilities {
